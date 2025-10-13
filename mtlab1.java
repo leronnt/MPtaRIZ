d138d730c8dfda0f935462b658830333c5e550a9
ler <udonbeh@gmail.com>
2025-10-13 23:17:43 +0300

    lab2 #4

--- mtlab1.java
+++ mtlab1.java
@@ -1,9 +1,65 @@
 class mtlab1 {
     public static void main(String[] args) {
-    System.out.println("Its works");
+        System.out.println("It's work");
+
+        int levels = 6; //этажи пирамиды
+        System.out.println("\n1) Ялинка у вигляді піраміди:");
+        printTree(levels);
+
+        System.out.println("\n2) Двовимірний масив:");
+        int rows = 3;
+        int cols = 5;
+        int startValue = 0;
+        int step = 3;
+
+        int[][] array = Array2D(rows, cols, startValue, step);
+        print2DArray(array);
+    }
+
+    //пирамида
+    public static void printTree(int levels) {
+        for (int i = 0; i < levels; i++) {
+            int stars = 2 * i + 1;      
+            int spaces = levels - i - 1; 
+
+            
+            for (int j = 0; j < spaces; j++) {
+                System.out.print("_");
+            }
+
+            
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
+
+    //2D масив создание
+    public static int[][] Array2D(int rows, int cols, int startValue, int step) {
+        int[][] arr = new int[rows][cols];
+        int value = startValue;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = value;
+                value += step;
+            }
+        }
+
+        return arr;
+    }
+
+    //2D масив
+    public static void print2DArray(int[][] arr) {
+        for (int[] row : arr) {
+            for (int num : row) {
+                System.out.printf("%8d", num);
+            }
+            System.out.println();
+        }
     }
 }
-
 
 
 
